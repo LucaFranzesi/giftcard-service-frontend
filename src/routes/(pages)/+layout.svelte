563850eabcd55488e210cb5769090dedcb60ed89
Luca Franzesi <luca.franzesi@beatdata.it>
2025-04-12 10:29:46 +0200

    Add Layouts for pages

---
+++ src/routes/(pages)/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { Navigation } from '@skeletonlabs/skeleton-svelte';
+  import { _ } from 'svelte-i18n';
+    // Icons
+    import IconMenu from '@lucide/svelte/icons/menu';
+    import IdCard from '@lucide/svelte/icons/id-card';
+    import IconImage from '@lucide/svelte/icons/image';
+    import IconMusic from '@lucide/svelte/icons/music';
+    import IconVideo from '@lucide/svelte/icons/video';
+    import IconSettings from '@lucide/svelte/icons/settings';
+  
+
+    let isExpansed = $state(true);
+
+    function toggleExpanded() {
+        isExpansed = !isExpansed;
+    }
+
+    let { children } = $props();
+    
+</script>
+
+<div class="grid grid-cols-[auto_1fr] h-[calc(100vh-2rem)]">
+    <Navigation.Rail expanded={isExpansed} tilesJustify="justify-top">
+      {#snippet header()}
+        <Navigation.Tile labelExpanded="Menu" onclick={toggleExpanded} title="Toggle Menu Width">
+          <IconMenu />
+        </Navigation.Tile>
+      {/snippet}
+      {#snippet tiles()}
+        <Navigation.Tile labelExpanded={$_('CARD_MENU_EXTENDED_LABEL') ?? "Carte"} href="/dashboard">
+          <IdCard />
+        </Navigation.Tile>
+        <Navigation.Tile labelExpanded="Browse Images" href="/images">
+          <IconImage />
+        </Navigation.Tile>
+        <Navigation.Tile labelExpanded="Browse Music" href="/music">
+          <IconMusic />
+        </Navigation.Tile>
+        <Navigation.Tile labelExpanded="Browse Videos" href="/videos">
+          <IconVideo />
+        </Navigation.Tile>
+      {/snippet}
+      {#snippet footer()}
+        <Navigation.Tile labelExpanded="Settings" href="/settings" title="Settings"><IconSettings /></Navigation.Tile>
+      {/snippet}
+    </Navigation.Rail>
+        <div class="flex items-center justify-center min-w-full">
+          {@render children()}
+        </div>
+    </div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import '../app.css';
+	import FlagPicker from '$lib/components/FlagPicker.svelte';
+	import { locale } from 'svelte-i18n';
+	import { isDark } from '$lib/stores/theme';
+	import { onMount } from 'svelte';
+	import { Switch } from '@skeletonlabs/skeleton-svelte';
+	import IconMoon from '@lucide/svelte/icons/moon';
+	import IconSun from '@lucide/svelte/icons/sun';
+
+	let { children } = $props();
+
+	var isLoading = $state(true);
+	let mode = $state(false);
+
+	onMount(() => {
+		const savedTheme = localStorage.getItem('theme');
+
+		let currLang = localStorage.getItem('language') || 'it-IT';
+		localStorage.setItem('language', currLang);
+		$locale = currLang;
+
+		if (savedTheme === 'dark') {
+			mode = true;
+			toggleTheme(false);
+		} else {
+			mode = false;
+			toggleTheme(true);
+		}
+
+		isLoading = false;
+	});
+
+	function toggleTheme(checkStatus: boolean) {
+		isDark.set(checkStatus);
+		localStorage.setItem('theme', checkStatus ? 'light' : 'dark');
+		mode = checkStatus;
+	}
+
+	let bgColor = 'dark:bg-primary-500 bg-surface-200';
+	let bgMenu = 'dark:bg-primary-500 bg-surface-200';
+</script>
+
+{#if !isLoading}
+	<div class="bg-surface fixed right-0 top-0 flex flex-row items-start space-x-1 p-5">
+		<Switch
+			controlActive="bg-primary-500"
+			checked={mode}
+			onCheckedChange={(e) => toggleTheme(e.checked)}
+		>
+			{#snippet inactiveChild()}<IconMoon size="14" />{/snippet}
+			{#snippet activeChild()}<IconSun size="14" />{/snippet}
+		</Switch>
+		<FlagPicker {bgColor} {bgMenu} />
+	</div>
+
+	<div class="flex h-full flex-col">
+		<div class="flex-grow">
+			{@render children()}
+		</div>
+	</div>
+
+	<footer
+		class="bg-primary-50-950 text-primary-950-50 fixed bottom-0 left-0 w-full p-1 text-right text-sm"
+	>
+		<p>&copy; {new Date().getFullYear()} Luca Franzesi. All rights reserved.</p>
+	</footer>
+{/if}
