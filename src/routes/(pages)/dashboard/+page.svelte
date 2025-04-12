3a4a03b7227b1ef6476c7b72ee6df931afa6f7c0
Luca Franzesi <luca.franzesi@beatdata.it>
2025-04-12 10:30:17 +0200

    Add test pages for navigation

---
+++ src/routes/(pages)/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let isExpansed = $state(true);
+
+	function toggleExpanded() {
+		isExpansed = !isExpansed;
+	}
+</script>
+
+<p class="bg-red-500">CONTENT</p>
---
+++ src/routes/(pages)/other/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Navigation } from '@skeletonlabs/skeleton-svelte';
+    // Icons
+    import IconMenu from '@lucide/svelte/icons/menu';
+    import IconFolder from '@lucide/svelte/icons/folder';
+    import IconImage from '@lucide/svelte/icons/image';
+    import IconMusic from '@lucide/svelte/icons/music';
+    import IconVideo from '@lucide/svelte/icons/video';
+    import IconSettings from '@lucide/svelte/icons/settings';
+
+    let isExpansed = $state(true);
+
+    function toggleExpanded() {
+        isExpansed = !isExpansed;
+    }
+  
+</script>
+
+<p class="bg-blue-500">
+  OTHER
+</p>
